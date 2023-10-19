d27464ccfa6862f4bb7216305ca3aef5eaa4620b
Alstro20 <sb74851@gmail.com>
2023-10-19 14:12:20 -0500

    got saving and loading working

--- MovieDatabase.py
+++ MovieDatabase.py
@@ -1,9 +1,76 @@
 import json
+import pickle
 
 class Movie:
+    name = ""
     genres = []
-    length = 0
-    rating = 0
+    length = 0 #in seconds
+    rating = 0 # 0-100, with 10 being equivalent to 1.0
 
-def add_movie(movieName):
-    movieName = Movie
+movies = []
+
+#broken JSON code
+
+# def load_movies():
+#     f = open(movies.json)
+#     movies = json.load(f)
+#     f.close()
+#     print("DEBUG: loaded movies")
+
+# def save_movies():
+#     with open('movies.json', 'w') as f:
+#         json.dump(movies, f)
+#     print("DEBUG: saved movies")
+
+
+def save_movies():
+    f = open('movies', 'wb')
+    pickle.dump(movies, f)
+    f.close()
+
+    print("DEBUG: movies saved")
+
+def load_movies():
+    f = open('movies', 'rb')
+    movies = pickle.load(f)
+    f.close()
+
+    print("DEBUG: movies loaded")
+
+
+def add_movie(movieNameStr=None, length=None, rating=None):
+    # TODO: Verify input types
+
+    # Get movie name if one is not provided
+    if movieNameStr == None:
+        print("Enter movie name")
+        movieNameStr = input()
+    
+    # Create object, replacing spaces with underscores and making lowercase, and retain the string as movieNameStr
+    movieObj = movieNameStr.replace(" ","_").lower()
+    movieObj = Movie()
+    movieObj.name = movieNameStr
+
+    # get length if not provided
+    if length == None:
+        print("Enter movie length in seconds")
+        movieObj.length = input()
+
+    # get rating if not provided
+    if rating == None:
+        print("Enter movie rating, 0-100")
+        movieObj.rating = input()
+
+    #add movie to list of movies
+    movies.append(movieObj)
+
+
+def listAllMovies():
+    for item in movies:
+        print(item.name)
+    print("DEBUG: Listed movies")
+
+load_movies()
+add_movie()
+listAllMovies()
+save_movies()
